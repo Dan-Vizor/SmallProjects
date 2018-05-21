24d847ce989b458f88b4d157669b44f4dd2995a1
Dan Vizor <danielv995.dv@gmail.com>
2018-05-21 10:32:54 +0100

    Update AI.py

--- projects/AI.py
+++ projects/AI.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python
 import os
 
+def comp(array,low,high):
+	loop = 0
+	out = ""
+	for cy in range(low,high):
+		out += array[cy]
+	return out
+
 while True:
+	### main input
+	Rinp = raw_input("> ")
+	###
+	Aimp = Rinp.split()
+	
+	if lower(Aimp[0]) == "find":
+		os.system("find " + comp(Aimp,1,len(Aimp)))
