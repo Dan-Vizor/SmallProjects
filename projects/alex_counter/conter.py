3a78faea9100b9fc50c08af7c26669eb7a337e8b
Dan-Vizor <danielv995.dv@gmail.com>
2018-08-11 21:33:09 +0100

    add counter

--- projects/alex_counter/conter.py
+++ projects/alex_counter/conter.py
@@ -7,13 +7,39 @@ except:
 	print("error: no data file")
 	exit()
 
-data = []
-for x in doc:
-	data += [x]
+#data = []
+#for x in doc:
+#	data += [x]
+data = doc.read(1)
 doc.close()
+print(data)
 
-file = open("/home/daniel/SmallProjects/projects/alex_counter/data.txt","w")
-out = str(data[0])
-file.write(str(int(out) + 1))
-print(out + " stupid things said sinse 9/8/18")
-file.close()
+rdata = str(data[0])
+print("alex is on " + rdata.rstrip())
+
+while True:
+	menu = raw_input(": ")
+
+	split = []
+	for x in menu:
+		split += [x]
+
+	if split[0] == "+":
+		file = open("/home/daniel/SmallProjects/projects/alex_counter/data.txt","w")
+		file.write(str(int(rdata) + int(split[1])))
+		print(rdata + " stupid things said sinse 9/8/18")
+		file.close()
+		break
+
+	elif split[0] == "-":
+		file = open("/home/daniel/SmallProjects/projects/alex_counter/data.txt","w")
+		file.write(str(int(rdata) - int(split[1])))
+		print(rdata + " stupid things said sinse 9/8/18")
+		file.close()
+		break
+
+	elif menu == "exit":
+		break
+
+	else:
+		print("error: invalid command")
