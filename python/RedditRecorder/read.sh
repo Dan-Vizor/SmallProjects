3a03ab7fbff743f0ad92efb004467e641027181c
Dan-Vizor <danielv995.dv@gmail.com>
2019-09-27 20:08:55 +0100

    update name

---
+++ python/RedditRecorder/read.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+while true
+do
+	clear
+	cat output.csv
+	sleep 5
+done
---
+++ python/RedditRecorder/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+touch input.txt
+echo $'Linux:YkVc0tgpIyzdlQ:V1.1 (by /u/local_meme_dealer45)\nYkVc0tgpIyzdlQ\n[client_secret]\n[username]\n[password]' > meta.txt
+wget -q 'https://raw.githubusercontent.com/Dan-Vizor/SmallProjects/master/python/RedditRecorder/RedditRecord.py'
+wget -q 'https://raw.githubusercontent.com/Dan-Vizor/SmallProjects/master/python/RedditRecorder/read.sh'
